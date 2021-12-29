4a00d6568bc602f67d3c7223d5954d1e041d88e8
root <1695219012@qq.com>
2021-12-29 19:02:22 +0800

    全部完成。不进行后续服务器部署的操作了，因为还要备案等等。

---
+++ blog-admin/src/main/java/com/mszlu/blog/admin/mapper/AdminMapper.java
@@ -0,0 +1,12 @@
+package com.mszlu.blog.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mszlu.blog.admin.pojo.Admin;
+import com.mszlu.blog.admin.pojo.Permission;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface AdminMapper extends BaseMapper<Admin> {
+
+}
---
+++ blog-admin/src/main/java/com/mszlu/blog/admin/service/SecurityUserService.java
@@ -0,0 +1,27 @@
+package com.mszlu.blog.admin.service;
+
+import com.mszlu.blog.admin.pojo.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+@Component
+public class SecurityUserService implements UserDetailsService {
+    @Autowired
+    private AdminService adminService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminService.findAdminByUsername(username);
+        if (admin == null) {
+            return null;
+        }
+        UserDetails userDetails = new User(username, admin.getPassword(), new ArrayList<>());
+
+        return userDetails;
+    }
+}
